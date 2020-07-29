a86d82c7c0468cc44c97fe4687983ad35870ddc4
luoqiufen <luoq6868@163.com>
2020-07-29 14:57:21 +0800

    mes1

--- model/models.js
+++ model/models.js
@@ -1,8 +1,24 @@
 /*
  * @Author: your name
  * @Date: 2020-07-29 09:45:56
- * @LastEditTime: 2020-07-29 09:45:56
+ * @LastEditTime: 2020-07-29 14:28:28
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \message\model\models.js
  */ 
+
+// 创建message对象的Model对象
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const msgSchema = new Schema({
+    username:String,
+    message:String,
+    date:String
+},{
+    collection:'message'//指定集合名称
+})
+
+module.exports = {
+    Message:mongoose.model('msg',msgSchema)
+}
--- router/message.js
+++ router/message.js
@@ -1,8 +1,18 @@
-/*
- * @Author: your name
- * @Date: 2020-07-29 09:47:15
- * @LastEditTime: 2020-07-29 09:47:16
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \message\router\message.js
- */ 
+ // 处理留言的路由
+const express = require('express');
+const Route = express.Router();
+
+//  /message请求
+Route.get('/',function(req,res){
+    res.render('index');
+})
+
+
+
+
+
+
+
+
+
+module.exports = Route;
